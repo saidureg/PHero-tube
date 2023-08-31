a868dd7b3379abef9da164633ff2989bf0e6a30c
saidureg <saidureg@gmail.com>
2023-08-31 22:27:31 +0600

    show the category tab from API data

--- script/index.js
+++ script/index.js
@@ -1,1 +1,17 @@
-console.log("connected");
+// show category using API
+const loadCategory = async () => {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await res.json();
+  const tabContainer = document.getElementById("tab-container");
+  data.data.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+          <a class="tab tab-active">${category.category} </a>
+          `;
+    tabContainer.appendChild(div);
+    console.log(category);
+  });
+};
+loadCategory();
